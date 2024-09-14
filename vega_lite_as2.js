6ac5d7b97d7d4d94fe6b2af868a6eee56e242f72
sai-ramishetty <sairamishetty9@outlook.com>
2024-09-14 17:36:12 +1000

    r

--- vega_lite_as2.js
+++ vega_lite_as2.js
@@ -1,28 +1,4 @@
-var vg_1 = {
-    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-    "description": "Time series graph representing net energy supply over time",
-    "width": 600,
-    "height": 400,
-    "data": {
-      "url": "data.csv"
-    },
-    "mark": "line",
-    "encoding": {
-      "x": {
-        "field": "Date",
-        "type": "temporal",
-        "timeUnit": "yearmonth",
-        "axis": {"format": "%b %Y", "title": "Date"}
-      },
-      "y": {
-        "field": "Net energy supply/use - total",
-        "type": "quantitative",
-        "title": "Net Energy Supply (PJ)"
-      }
-    },
-    "title": "Net Energy Supply in Australia Over Time"
-  };
-  
-  vegaEmbed("#bar_chart", vg_1).then(function(result) {
+var vg_1 = "barchart.json"
+vegaEmbed("#barchart", vg_1).then(function(result) {
     console.log("Chart rendered successfully.");
   }).catch(console.error);
